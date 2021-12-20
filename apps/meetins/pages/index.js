d12dbce16688e5e9790ba90c1552cc60ce998d39
John Cardwell <cardwell.john@gmail.com>
2021-12-20 14:58:34 -0800

    remove old auth0 pages

--- apps/meetins/pages/index.js
+++ apps/meetins/pages/index.js
@@ -1,46 +1,57 @@
+import { useUser } from '@auth0/nextjs-auth0';
 import styles from './index.module.scss';
 import { Card, CardContent } from '@material-ui/core';
 
 import Link from 'next/link';
 
 export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
-  return (
-    <div className={styles.page}>
-      <div className="wrapper">
-        <div className="container">
-          
-          <div id="welcome">
-            <h1>
-              <span> Hello there, </span>
-              Welcome meetins 👋
-            </h1>
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>{error.message}</div>;
+
+  console.log('user', user);
+
+  if (user) {
+    return (
+      <div className={styles.page}>
+        <div className="wrapper">
+          <div className="container">
+            
+            <div id="welcome">
+              <h1>
+                <span> Hello there, {user.given_name}</span>
+                Welcome to meetins 👋
+              </h1>
+            </div>
+  
+            <Card>
+              <CardContent>
+                <h2>Card Content</h2>
+                <div>
+                  <Link href="/about">
+                    Go to About page
+                  </Link>
+                </div>
+                <div>
+                  <Link href="/users">
+                    Go to Users page
+                  </Link>
+                </div>
+              </CardContent>
+            </Card>
+
+            <a href="/api/auth/logout">Logout</a>
           </div>
-
-          <Card>
-            <CardContent>
-              <h2>Card Content</h2>
-              <div>
-                <Link href="/about">
-                  Go to About page
-                </Link>
-              </div>
-              <div>
-                <Link href="/users">
-                  Go to Users page
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
-
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
+
+
 }
 
 export default Index;
