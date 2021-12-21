4ffd89260f9d10f8a60ee67fc8e04f474c2c17bf
John Cardwell <cardwell.john@gmail.com>
2021-12-21 12:13:28 -0800

    remove generated root component from cypress

--- apps/meetins-ui-shared-e2e/src/integration/meetins-ui-shared/meetins-ui-shared.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('meetins-ui-shared: MeetinsUiShared component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=meetinsuishared--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to MeetinsUiShared!');
-    });
-});
