6350b3e4005bdd1e862e7f2559f47c8a82d1e7bf
John Cardwell <cardwell.john@gmail.com>
2021-12-21 14:18:25 -0800

    fix e2e tests

--- apps/meetins-ui-shared-e2e/src/integration/mheader/mheader.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('meetins-ui-shared: MHeader component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=mheader--primary&args=title:Sample Title;'));
-    
-    it('should render the component', () => {
-      cy.get('h1.MuiTypography-root').should('contain', 'Sample Title');
-    });
-});
