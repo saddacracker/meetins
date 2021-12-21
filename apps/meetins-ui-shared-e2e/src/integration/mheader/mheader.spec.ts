39bc6d967105b01f9da2c487cc007d9c541fd25b
John Cardwell <cardwell.john@gmail.com>
2021-12-21 11:32:12 -0800

    fix e2e test and remove unused file

--- apps/meetins-ui-shared-e2e/src/integration/mheader/mheader.spec.ts
+++ apps/meetins-ui-shared-e2e/src/integration/mheader/mheader.spec.ts
@@ -1,7 +1,7 @@
 describe('meetins-ui-shared: MHeader component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=mheader--primary&args=title;'));
+  beforeEach(() => cy.visit('/iframe.html?id=mheader--primary&args=title:Sample Title;'));
     
     it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to MHeader!');
+      cy.get('h1.MuiTypography-root').should('contain', 'Sample Title');
     });
 });
