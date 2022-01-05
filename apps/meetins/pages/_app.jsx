c74e827f2ca223a5e1a97401700cb1acd78455a1
John Cardwell <cardwell.john@gmail.com>
2022-01-04 16:36:14 -0800

    add material icons pkg and wrap app bar title with a link to homepage

--- apps/meetins/pages/_app.jsx
+++ apps/meetins/pages/_app.jsx
@@ -1,12 +1,12 @@
 import { UserProvider } from '@auth0/nextjs-auth0';
 import Head from 'next/head';
-
+import Link from "next/link";
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import './styles.css';
 
@@ -30,14 +30,17 @@ function CustomApp({ Component, pageProps }) {
       <Head>
         <title>Welcome to meetins!</title>
       </Head>
+
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            {/* <MenuIcon /> */}
+            <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Meetins
-          </Typography>
+            <Typography variant="h6" className={classes.title}>
+              <Link href="/">
+                  Meetins
+              </Link>
+            </Typography>
           {/* {auth && (
             <div>
               <IconButton
